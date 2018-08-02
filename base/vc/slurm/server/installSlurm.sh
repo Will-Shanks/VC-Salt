4866bbab390de65d2528bbec7f3cb7d1735e4d78
Will Shanks <shanks@ucar.edu>
2018-08-02 09:36:45 -0600

    updated service file to reflect location of slurm files

--- base/vc/slurm/server/installSlurm.sh
+++ base/vc/slurm/server/installSlurm.sh
@@ -2,7 +2,7 @@
 
 PMIX_DIR='/usr/local/pmix/2.1.1/'
 SLURM_DIR='/usr/local/slurm/17.11.5.1/'
-OMPI_DIR='/usr/local/ompi/3.1.0/'
+OMPI_DIR='/usr/local/openmpi/3.1.0/'
 #PMIX_SHA=`(find $PMIX_DIR-type f -print0  | sort -z | xargs -0 sha1sum;
 #find $PMIX_DIR \( -type f -o -type d \) -print0 | \
 # sort -z | xargs -0 stat -c '%n %a') | sha1sum`
